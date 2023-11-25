6d4c05032cc0e668cb395fca58b84363af54f4d5
Yuanhsi Chung <freshliver.cys@gmail.com>
2023-11-26 02:11:44 +0800

    Connect the outputs of ALUControl and 2 rs MUX to ALU

--- src/main/scala/riscv/core/Execute.scala
+++ src/main/scala/riscv/core/Execute.scala
@@ -37,7 +37,9 @@ class Execute extends Module {
   alu_ctrl.io.funct7 := funct7
 
   // lab3(Execute) begin
-
+  alu.io.op1  := Mux(io.aluop1_source === 0.U, io.reg1_data, 0.U(Parameters.DataWidth))
+  alu.io.op2  := Mux(io.aluop2_source === 1.U, io.immediate, io.reg2_data)
+  alu.io.func := alu_ctrl.io.alu_funct
   // lab3(Execute) end
 
   io.mem_alu_result := alu.io.result
